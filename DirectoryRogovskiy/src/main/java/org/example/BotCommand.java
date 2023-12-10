9117364f6d3c673d67b6ef1ae6a9aa2bfd293063
Константин Роговский <r_kostya2001@mail.ru>
2023-12-11 00:40:42 +0800

    RogProg2

---
+++ DirectoryRogovskiy/src/main/java/org/example/BotCommand.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public enum BotCommand {
+    ПОГОДА("/погода"),
+    КОМАНДЫ("/команды"),
+    СТАРТ("/start");
+    //ГОРОДА("/города");
+
+    private final String commandText;
+
+    BotCommand(String commandText) {
+        this.commandText = commandText;
+    }
+
+    public String getCommandText() {
+        return commandText;
+    }
+}
