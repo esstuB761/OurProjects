96bd9f05fd995bd7aa481d408cb8e846f9a9a4b5
komorreby <balyadorzhieva@gmail.com>
2023-12-10 22:35:34 +0800

    balzhina (mysolution)

---
+++ DorzhievaBalzhina/src/main/java/org/example/BotCommand.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public enum BotCommand {
+    ПОГОДА("/погода"),
+    КОМАНДЫ("/команды"),
+    ФИО("/фио"),
+    СТАРТ("/start");
+
+
+    private final String commandText;
+
+    BotCommand(String commandText) {
+        this.commandText = commandText;
+    }
+
+    public String getCommandText() {
+        return commandText;
+    }
+}
