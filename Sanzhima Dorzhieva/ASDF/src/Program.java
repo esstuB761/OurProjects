125a753b6d9999ab936b3f4805a6fe7c9664291b
fghj <s90644@esstu.onmicrosoft.com>
2023-12-21 22:25:57 +0800

    Добавила задание

---
+++ Sanzhima Dorzhieva/ASDF/src/Program.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class Program {
+    public static void main (String [] args){
+        String answer;
+        boolean exit;
+        System.out.println("Что это такое: синий, большой, с усами и полностью набит зайцами?");
+        Scanner inputAnswer = new Scanner(System.in);
+        exit=false;
+
+        for(int i=1; i<=3; i++){
+            if (exit == true)
+                break;
+            answer= inputAnswer.next();
+            switch (answer){
+                case ("Троллейбус"):
+                    System.out.println("Правильно");
+                    exit=true;
+                    break;
+                case ("Сдаюсь"):
+                    System.out.println("Правильный ответ Троллейбус");
+                    exit=true;
+                    break;
+                default:
+                    System.out.println("Подумай еще");
+            }
+
+        }
+    }
+}
